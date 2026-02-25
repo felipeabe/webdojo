2eee1a2344dd29b74f5e601e587686ec07615733
felipehabe <felipehabe301@gmail.com>
2026-02-25 18:00:38 -0300

    testes iniciais

---
+++ web/cypress/e2e/login.cy.js
@@ -0,0 +1,27 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.viewport(1440, 900);
+    cy.visit("http://localhost:3000");
+  });
+});
+
+describe("Login", () => {
+  it("Deve logar com sucesso", () => {
+    cy.viewport(1440, 900);
+    cy.visit("http://localhost:3000");
+    cy.get("#email").type("papito@webdojo.com");
+    cy.get("#password").type("katana123");
+    cy.contains("button", "Entrar").click();
+
+    cy.get('[data-cy="user-name"]')
+      .should("be.visible")
+      .and("have.text", "Fernando Papito");
+
+    cy.get('[data-cy="welcome-message"]')
+      .should("be.visible")
+      .and(
+        "have.text",
+        "Olá QA, esse é o seu Dojo para aprender Automação de Testes.",
+      );
+  });
+});
